e6a5cf579a541e7c71d27b32dab6c798ad0d2db2
Michael Tamaki <michaeltamaki@berkeley.edu>
2018-01-20 17:59:56 -0800

    Create assets folder

--- results.php
+++ results.php
@@ -4,23 +4,25 @@
     <meta charset="utf-8">
     <title>Davis Pantry</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="assets/css/style.css">
   </head>
 
     <body>
-      <?php include("includes/header.html");?>
+      <?php include("assets/includes/header.html");?>
 
   <!-- Login Section -->
       <section id="home-section" class="mb-5">
           <div class="container">
             <div class="row">
-                <h1>Results</h1>
+                <div class="col">
+                    <h1>Results for </h1>
+                </div>
             </div>
         </div>
 
     </section>
 
-    <?php include("includes/footer.html");?>
+    <?php include("assets/includes/footer.html");?>
 
     </body>
   </html>
